327ba9d2192fe5492af8c686eb07858665ab4689
Alikhan <muhammedalikhan27@gmail.com>
2024-02-14 23:28:18 +0530

    Added the functionality to render the task list in App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import AddTask from './components/AddTask/AddTask';
 import ViewTasks from './components/ViewTasks/ViewTasks';
-
+import Task from './components/Task/Task';
 function App() {
+  const [data, setData] = useState([]);
+  let id = 1;
+  function handleAddTask(task) {
+    setData([{ ...task, id: id }, ...data]);
+    id += 1;
+  }
+  
   return (
     <div className="App">
       <h1>Task Management App</h1>
-      <AddTask />
-      <ViewTasks></ViewTasks>
+      <AddTask onAddTask={handleAddTask} />
+      <ViewTasks>
+        {data.map(({ title, description }, index) => {
+          
+          return <Task key={index} title={title} description={description}></Task>
+        })}
+        {!data.length && 'No Task Available.!'}
+      </ViewTasks>
     </div>
   );
 }
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Task Management App/i);
+  const h1 = screen.getByRole('heading', {level: 1});
+  expect(h1).toBeInTheDocument();
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders No Task Available', () => {
+  render(<App />);
+  const el = screen.getByText(/No task available/i);
+  expect(el).toBeInTheDocument();
+});
+
+test('should rended the first task list', () => {
+  render(<App />);
+  const input = screen.getByLabelText('Title', { selector: 'input' });;
+  const textarea = screen.getByLabelText('Description', { selector: 'textarea' });
+  const AddTaskBtn = screen.getByTestId('add-task');
+  act(() => {
+    userEvent.type(input, 'Testing 1');
+    userEvent.type(textarea, 'Testing the description content');
+    userEvent.click(AddTaskBtn)
+  });
+  const titleEl = screen.getByText('Testing 1');
+  const descEl = screen.getByText('Testing the description content');
+  expect(titleEl).toBeInTheDocument();
+  expect(descEl).toBeInTheDocument();
+})
