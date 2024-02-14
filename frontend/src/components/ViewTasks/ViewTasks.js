c9fa536968df8a5f35f9638dc2e99649f4f16b98
Alikhan <muhammedalikhan27@gmail.com>
2024-02-14 23:07:59 +0530

    Added the HTML for View Task Component

--- frontend/src/components/ViewTasks/ViewTasks.js
+++ frontend/src/components/ViewTasks/ViewTasks.js
@@ -1,5 +1,8 @@
-export default function ViewTasks() {
+export default function ViewTasks({children}) {
     return (<div className="view-task-container">
         <h2>All Tasks</h2>
+        <ul className="task-list">
+            {children}
+        </ul>
     </div>)
 }
