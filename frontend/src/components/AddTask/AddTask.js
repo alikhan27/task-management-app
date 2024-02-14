142edd6df5b37a789b57ae65d0f853ca1472db76
Alikhan <muhammedalikhan27@gmail.com>
2024-02-14 16:50:31 +0530

    Added the form fields for the Add Task Component

--- frontend/src/components/AddTask/AddTask.js
+++ frontend/src/components/AddTask/AddTask.js
@@ -1,5 +1,18 @@
 export default function AddTask() {
     return (<div className="add-task-container">
         <h2>Add Task</h2>
+        <form>
+            <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input type="text" className="form-control" name="title" id="title" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="desc">Description</label>
+                <textarea className="form-control" name="description" id="desc" />
+            </div>
+            <div className="btn-group">
+                <button className="btn" data-testid="add-task">Add Task</button>
+            </div>
+        </form>
     </div>)
 }
--- frontend/src/components/AddTask/AddTask.test.js
+++ frontend/src/components/AddTask/AddTask.test.js
@@ -7,4 +7,29 @@ describe("It should render the Add Task Component", () => {
         const heading = screen.getByRole('heading', {level: 2})
         expect(heading).toHaveTextContent('Add Task');
     })
+    it('should render the label as Title', () => {
+        render(<AddTask />);
+        const label = screen.getByLabelText('Title');
+        expect(label).toBeInTheDocument();
+    })
+    it('should render the input field for Title', () => {
+        render(<AddTask />);
+        const input = screen.getByLabelText('Title', {selector: 'input'});
+        expect(input).toBeInTheDocument();
+    })
+    it('should render the label as Description', () => {
+        render(<AddTask />);
+        const label = screen.getByLabelText('Description');
+        expect(label).toBeInTheDocument();
+    })
+    it('should render the textarea field for Description', () => {
+        render(<AddTask />);
+        const input = screen.getByLabelText('Description', {selector: 'textarea'});
+        expect(input).toBeInTheDocument();
+    })
+    it('should render the button with text Add Task', () => {
+        render(<AddTask />);
+        const input = screen.getByTestId('add-task');
+        expect(input).toBeInTheDocument();
+    })
 })
