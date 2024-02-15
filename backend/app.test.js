b41fd7b6381bfe571f2a1266ab1cd8c8acabfce4
Alikhan <muhammedalikhan27@gmail.com>
2024-02-15 19:09:39 +0530

    Added the GET method for tasks by ID

--- backend/app.test.js
+++ backend/app.test.js
@@ -12,4 +12,25 @@ describe("GET /", () => {
                 );
             }))
     })
+
+    it('GET /api/task/1 => items by ID', () => {
+        return request(app)
+          .get('/api/task/1')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toEqual(
+              expect.objectContaining({
+                title: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+      });
+
+      it('GET /api/task/1 => items by ID not found', () => {
+        return request(app)
+          .get('/api/task/1000000')
+          .expect(404)
+      });
 })
--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
 
-const tasks = [];
+const tasks = [{title: 'John Doe', description: 'Teaching science'}];
 
 router.get('/api/tasks', (req, res) => {
   res.json(tasks);
+});
+
+router.get('/api/task/:id', (req, res, next) => {
+    const task = tasks.find(
+        (task, index) => String(index+1) === String(req.params.id)
+        );
+
+    if (!task) {
+        return next(createError(404, 'Not Found'));
+      }
+    
+      res.json(task);
 });
 
 module.exports = router;
